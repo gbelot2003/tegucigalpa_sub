1e364b74e272d28aed5eab504c02b06facd3f267
gbelot2003 <gerardobelot@outlook.com>
2014-08-02 03:16:37 +0000

    some css changes

--- templates/pages/footer.tpl.php
+++ templates/pages/footer.tpl.php
@@ -1,42 +1,43 @@
  <footer class="l-footer panel" role="contentinfo">
     <div class="row">	
 		<div class="large-12 columns logos-images">
-			<div class="row">
-				<div class="large-1 medium-6 small-12 columns">
+				<div class="sponsor1">
 					<ul class="logos-li">
-						<li class="premium"><a class="canaturh" href="#">Canaturh</a></li>
+						<li class="premium"><a class="canaturh" href="http://www.canaturh.org/">Canaturh</a></li>
+						<li class="lpremium"><a class="iht" href="http://www.iht.hn/">IHT</a></li>
 					</ul>
 				</div>
-				<div class="large-2 medium-6 small-12 columns">
-					<ul class="logos-li">	
-						<li class="lpremium"><a class="iht" href="#">IHT</a></li>
-					</ul>
-				</div>
-				<div class="large-2 medium-6 small-12 columns">
+				<div class="sponsor2">
 					<ul class="logos-li">
-						<li class="sponser"><a class="usaid" href="#">usaid</a></li>
-						<li class="sponser"><a class="nwt" href="#">New World Trips</a></li>
+						<li class="lpremium-2"><a class="usaid" href="http://www.usaid-proparque.org/">usaid</a></li>
+						<li class="lpremium-3"><a class="nwt" href="http://www.nwtrips.com/">New World Trips</a></li>
 					</ul>
 				</div>
-				<div class="large-2 medium-6 small-12 columns">
+				<div class="sponsor3">
 					<ul class="logos-li">
-						<li class="sponser"><a class="patrocinador" href="#">patrocinador</a></li>
-						<li class="sponser"><a class="patrocinador" href="#">patrocinador</a></li>
+						<li class="sponsor"><a class="imc" href="http://www.imcmedios.com/">patrocinador</a></li>
+						<li class="sponsor"><a class="divesa" href="http://www.divesahn.com/">patrocinador</a></li>
+
 					</ul>
 				</div>
-				<div class="large-2 medium-6 small-12 columns">
+				<div class="sponsor3">
 					<ul class="logos-li">
-						<li class="sponser"><a class="patrocinador" href="#">patrocinador</a></li>
-						<li class="sponser"><a class="patrocinador" href="#">patrocinador</a></li>
+						<li class="sponsor"><a class="patrocinador" href="#">patrocinador</a></li>
+						<li class="sponsor"><a class="patrocinador" href="#">patrocinador</a></li>						
 					</ul>
 				</div>
-				<div class="large-2 medium-6 small-12 columns end">
+				<div class="sponsor3">
 					<ul class="logos-li">
-						<li class="sponser"><a class="patrocinador" href="#">patrocinador</a></li>
-						<li class="sponser"><a class="patrocinador" href="#">patrocinador</a></li>
+						<li class="sponsor"><a class="patrocinador" href="#">patrocinador</a></li>
+						<li class="sponsor"><a class="patrocinador" href="#">patrocinador</a></li>						
 					</ul>
 				</div>
-			</div>
+				<div class="sponsor3">
+					<ul class="logos-li">
+						<li class="sponsor"><a class="patrocinador" href="#">patrocinador</a></li>
+						<li class="sponsor"><a class="patrocinador" href="#">patrocinador</a></li>						
+					</ul>
+				</div>
 		</div>
 	</div>
     <div class="row">
